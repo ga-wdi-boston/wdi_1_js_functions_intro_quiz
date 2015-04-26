699494c5b4171c39e34cec7f63be0f5acb237c0f
Antony Donovan <antony.donovan@generalassemb.ly>
2015-04-26 17:28:53 -0400

    Instructions and script

---
+++ README.md
@@ -0,0 +1,9 @@
+# Quiz: Introduction to JavaScript Functions
+
+#### Directions
+
+* Fork and clone this quiz.
+* Write your answers in the `quiz.js` file.
+* You have 20 minutes. When you're done, commit your work and make a pull request.
+* Feel free to use your notes and Google, but you should work by yourself (not with a partner).
+* Also feel free to run your code in node to check if it works!
---
+++ quiz.js
@@ -0,0 +1,74 @@
+// Quiz: Introduction to JavaScript Functions
+
+// Question 1
+// write a function square(x) that returns the argument passed multiplied by itself
+// then write the functions cube(x), x times x times x,
+// and quad(x), x times x times x times x, using the function square(x)
+// verify that square(2) === 4, cube(3) === 27, and that quad(4) === 64
+/* your code starts here */
+function square(x) {
+  return x*x;
+}
+function cube(x) {
+  return square(x)*x;
+}
+function quad(x) {
+  return square(x)*square(x);
+}
+/* your code ends here */
+
+//Question 1 check
+if (square(2) !== 4 || cube(3) !== 27 || quad(4) !== 256) {
+  console.log("check question 1");
+}
+
+// Question 2
+// write a function sum(numbers, twiceOrHalf).
+// It should expect an array of numbers as the first argument
+// and an optional flag, twiceOrHalf, as the second argument.
+// If the flag is undefined, sum should return the total of the numbers in the array
+// If the flag is truthy, it should return twice that sum
+// If the flag is falsy but not undefined, it should return half that sum
+
+/* your code starts here */
+function sum(numbers, twiceOrHalf) {
+  var total = 0;
+  for (var i = 0; i < numbers.length; i++) {
+    total += numbers[i];
+  };
+  if (twiceOrHalf) {
+    total *= 2;
+  } else if (twiceOrHalf !== undefined) {
+    total /= 2;
+  }
+  return total;
+}
+/* your code ends here */
+
+//Question 2 check
+var nums = [2, 4, 6];
+if (sum(nums) !== 12 || sum(nums, false) !== 6 || sum(nums, !undefined) !== 24) {
+  console.log("check question 2");
+}
+
+
+// Question 3
+// write a function, max() that loops through zero or more arguments
+// and returns the largest number in that list
+/* your code starts here */
+function max() {
+  var maxium = arguments[0];
+  for (var i = 1; i < arguments.length; i++) {
+    if (maxium < arguments[i]) {
+    maxium = arguments[i];
+    }
+  }
+  return maxium;
+};
+/* your code ends here */
+
+//Question 3 check
+if (max() !== undefined || max(15) !== 15 ||
+    max(-1, 0) !== 0 || max(-1, -7, -4) !== -1) {
+  console.log("check question 3");
+}
