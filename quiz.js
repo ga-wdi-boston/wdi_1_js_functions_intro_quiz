9615681b7e3137a71ccca3b8dc67b2aa570e7669
Antony Donovan <antony.donovan@generalassemb.ly>
2015-04-26 17:32:32 -0400

    Quiz prep

--- quiz.js
+++ quiz.js
@@ -5,16 +5,9 @@
 // then write the functions cube(x), x times x times x,
 // and quad(x), x times x times x times x, using the function square(x)
 // verify that square(2) === 4, cube(3) === 27, and that quad(4) === 64
+
 /* your code starts here */
-function square(x) {
-  return x*x;
-}
-function cube(x) {
-  return square(x)*x;
-}
-function quad(x) {
-  return square(x)*square(x);
-}
+
 /* your code ends here */
 
 //Question 1 check
@@ -31,18 +24,7 @@ if (square(2) !== 4 || cube(3) !== 27 || quad(4) !== 256) {
 // If the flag is falsy but not undefined, it should return half that sum
 
 /* your code starts here */
-function sum(numbers, twiceOrHalf) {
-  var total = 0;
-  for (var i = 0; i < numbers.length; i++) {
-    total += numbers[i];
-  };
-  if (twiceOrHalf) {
-    total *= 2;
-  } else if (twiceOrHalf !== undefined) {
-    total /= 2;
-  }
-  return total;
-}
+
 /* your code ends here */
 
 //Question 2 check
@@ -56,15 +38,7 @@ if (sum(nums) !== 12 || sum(nums, false) !== 6 || sum(nums, !undefined) !== 24) 
 // write a function, max() that loops through zero or more arguments
 // and returns the largest number in that list
 /* your code starts here */
-function max() {
-  var maxium = arguments[0];
-  for (var i = 1; i < arguments.length; i++) {
-    if (maxium < arguments[i]) {
-    maxium = arguments[i];
-    }
-  }
-  return maxium;
-};
+
 /* your code ends here */
 
 //Question 3 check
