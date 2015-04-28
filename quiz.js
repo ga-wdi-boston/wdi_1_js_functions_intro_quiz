d9d624dd3f30c117046ec72f90ce7e3cde695ad8
Kinjalc <kinjal.chauhan@gmail.com>
2015-04-28 13:17:55 -0400

    git commit

--- quiz.js
+++ quiz.js
@@ -7,6 +7,17 @@
 // verify that square(2) === 4, cube(3) === 27, and that quad(4) === 64
 
 /* your code starts here */
+function square(x) {
+    console.log(x*x);
+    }
+
+function cube(x) {
+    console.log(x*x*x);
+    }
+
+function quad(x) {
+    console.log(square(x)*square(x));
+    }
 
 /* your code ends here */
 
@@ -16,15 +27,17 @@ if (square(2) !== 4 || cube(3) !== 27 || quad(4) !== 256) {
 }
 
 
-// Question 2
-// write a function sum(numbers, twiceOrHalf).
-// It should expect an array of numbers as the first argument
-// and an optional flag, twiceOrHalf, as the second argument.
-// If the flag is undefined, sum should return the total of the numbers in the array
-// If the flag is truthy, it should return twice that sum
-// If the flag is falsy but not undefined, it should return half that sum
+function sum(numbers, twiceOrhalf){
+    var x = numbers.add;
+    /*iknow the function above is wrong*/
 
-/* your code starts here */
+    if(twiceOrHalf == undefined){
+        return }
+        if (twiceOrHalf !== false){
+            return 2*y
+}
+        if
+
 
 /* your code ends here */
 
@@ -33,6 +46,8 @@ var nums = [2, 4, 6];
 if (sum(nums) !== 12 || sum(nums, false) !== 6 || sum(nums, !undefined) !== 24) {
   console.log("check question 2");
 }
+
+
 
 
 // Question 3
@@ -48,3 +63,4 @@ if (max() !== undefined || max(15) !== 15 ||
     max(-1, 0) !== 0 || max(-1, -7, -4) !== -1) {
   console.log("check question 3");
 }
+
